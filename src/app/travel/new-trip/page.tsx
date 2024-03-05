579679225f404e5b642879bac30455cf51d990a6
Lxdovic <ludovicdebever0@gmail.com>
2024-03-05 14:45:35 +0100

    feat(leaflet): add leaflet and basic map with marker

---
+++ src/app/travel/new-trip/page.tsx
@@ -0,0 +1,9 @@
+import { LazyMap } from "@/components/map/map-lazy";
+
+export default function Page() {
+  return (
+    <div className="h-96 overflow-hidden">
+      <LazyMap />
+    </div>
+  );
+}
