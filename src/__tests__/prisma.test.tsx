12c6386f67ac7fa66ecd64bbc25cec27adfc75fd
Julien SIMONET <julien@alobees.com>
2024-04-09 15:24:52 +0200

    feat(prisma): basic implementation done

--- src/__tests__/prisma.test.tsx
+++ src/__tests__/prisma.test.tsx
@@ -1,10 +1,12 @@
 import { expect, test, vi } from "vitest";
-import { createCity } from "@/lib/actions/createCity";
-import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
 vi.mock("../lib/prisma");
 
 test("createCity should return the new city created", async () => {
-  const newCity: Prisma.CitiesCreateInput = { name: "test", postalCode: 12345 };
+  const newCity: Prisma.UserCreateInput = {
+    name: "test",
+    password: "password",
+  };
+  expect(newCity.name).toEqual("test");
 });
---
+++ src/lib/actions/createUser.ts
@@ -0,0 +1,11 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../prisma";
+
+export async function createUser(newUser: Prisma.UserCreateInput) {
+  return await prisma.user.create({
+    data: {
+      name: newUser.name,
+      password: newUser.password,
+    },
+  });
+}
