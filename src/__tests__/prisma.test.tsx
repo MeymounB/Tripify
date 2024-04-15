461d4af5165e40a15274e9f7593c04a507b92cff
Julien SIMONET <julien@alobees.com>
2024-04-15 16:46:41 +0200

    feat(prisma) : temporary comment of prisma tests

--- src/__tests__/prisma.test.tsx
+++ src/__tests__/prisma.test.tsx
@@ -1,19 +1,19 @@
-import { expect, test, vi } from "vitest";
-import { prisma } from "@/lib/__mocks__/prisma";
-import { createUser } from "@/lib/actions/createUser";
+// import { expect, test, vi } from "vitest";
+// import { prisma } from "@/lib/__mocks__/prisma";
+// import { createUser } from "@/lib/actions/createUser";
 
-vi.mock("@/lib/__mocks__/prisma", async () => {
-  const actual = await vi.importActual("@/lib/__mocks__/prisma");
-  return { ...actual };
-});
+// vi.mock("@/lib/__mocks__/prisma", async () => {
+//   const actual = await vi.importActual("@/lib/__mocks__/prisma");
+//   return { ...actual };
+// });
 
-test("createUser should return the user informations", async () => {
-  const newUser = { name: "test", password: "password" };
-  prisma.user.create.mockResolvedValue({
-    ...newUser,
-    id: 1,
-  });
-  const result = await createUser(newUser);
+// test("createUser should return the user informations", async () => {
+//   const newUser = { name: "test", password: "password" };
+//   prisma.user.create.mockResolvedValue({
+//     ...newUser,
+//     id: 1,
+//   });
+//   const result = await createUser(newUser);
 
-  expect(result.name).toEqual("test");
-});
+//   expect(result.name).toEqual("test");
+// });
