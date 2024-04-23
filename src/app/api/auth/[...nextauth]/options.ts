4abe910963be243c7f724f16b91be2d670d3d070
Eliott Regnier <eliott2610@gmail.com>
2024-04-23 14:55:11 +0200

    next-auth sign in. Sign up only works with google at the moment

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,58 @@
+import type { NextAuthOptions } from 'next-auth'
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+          username: {
+            label: "Username:",
+            type: "text",
+            placeholder: "your-username"
+          },
+          password:{
+            label: "Password: ",
+            type: "password",
+            placeholder: "your-password"
+          }
+      },
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const res = await fetch("/your/endpoint", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const user = await res.json()
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    })
+  ],
+  // pages: { // optional
+  //   signIn: "/signin"
+  // }
+}
